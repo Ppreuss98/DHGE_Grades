aa015d332f2851194f849a1627891813862cb9fa
Julius Junfert <julius.junfert@accenture.com>
2019-03-18 17:40:49 +0100

    modify pom

--- projekt1/src/main/java/com/projekt/App.java
+++ projekt1/src/main/java/com/projekt/App.java
@@ -1,5 +1,11 @@
 package com.projekt;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        WebClient webClient = new WebClient();
+        HtmlPage page = null;
+        try {
+            page = webClient.getPage("https://gera.dhge.de/SelfService/start.php");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        HtmlForm form = page.getForms().get(0);
+        System.out.println(form);
+        String user = "123";
+        String pw = "123";
+
+        form.getInputByName("matrnr").setValueAttribute(user);
+        form.getInputByName("passw").setValueAttribute(pw);
+        try {
+            HtmlPage page2 = form.getButtonByName("Notenauskunft (Bildschirm)").click();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
